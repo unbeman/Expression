cff14ca1fa873d4d55dd0116f6e80b0cedde6da2
a.kukuev <a.kukuev@akukuev.local>
2016-12-21 14:13:44 +0300

    readd func IsComplete

--- Scanner.h
+++ Scanner.h
@@ -12,7 +12,7 @@ public:
     Scanner(const std::string& str): str(str) {}
     char operator*() const { return (char) ((pos <= str.size()) ? str[pos] : '\0'); }// проверка на переполнение
     Scanner& operator++() { pos++; return *this; }
-    bool IsComplete() const{ return (pos > str.size());}
+    bool IsComplete() const{ return (pos >= str.size());}
 private:
     const std::string& str;
     size_t pos = 0;
